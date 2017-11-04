2d8b1b19536041b8512bdcf05030bf8c336a5952
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2017-11-04 16:09:14 +0200

    helper docker scripts; ant design react ui library

---
+++ docker-scripts/cleanup.bat
@@ -0,0 +1,5 @@
+REM Fix scripts
+
+REM docker stop $(docker ps --filter ancestor=stylehopper/idontknow )
+REM docker rm $(docker ps --filter ancestor=stylehopper/idontknow )
+REM docker rmi stylehopper/idontknow
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import './App.css'
 import { applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import ReduxThunk from 'redux-thunk'
@@ -19,7 +20,7 @@ const store = createStoreWithMiddleware(window.__INITIAL_STATE__);
 const AppComponent = () => (
   <Provider store={store}>
     <Router>
-      <div>
+      <div className="App">
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>

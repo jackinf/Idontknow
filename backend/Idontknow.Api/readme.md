f98edbda6c42f09d0bd35a48266867c94e24b0c8
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2017-11-15 21:26:29 +0200

    [DDD]: readme

---
+++ backend/Idontknow.Api/readme.md
@@ -0,0 +1,7 @@
+## TODO
+
+- Fluent validation in services or repositories
+- Update something
+- 2+ operations in one transaction
+- Unit tests
+- File upload
---
+++ backend/Idontknow.DAL/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Idontknow.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private IDbContextTransaction _transaction;
+        
+        public ApplicationDbContext Context { get; private set; }
+
+        public UnitOfWork()
+        {
+            Context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+        }
+
+        public void SaveChanges() => Context.SaveChanges();
+        
+        private bool _isDisposed;
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!_isDisposed && isDisposing)
+                Context.Dispose();
+            _isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+        public void BeginTransaction() => _transaction = Context.Database.BeginTransaction();
+        public void Commit() => _transaction.Commit();
+        public void Rollback() => _transaction.Rollback();
+    }
+}
